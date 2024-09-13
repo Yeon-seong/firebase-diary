92eeb3aa5952161d971f7dce602ff0d67dce7f04
yeon-seong <dd300@naver.com>
2024-09-13 13:03:13 +0900

    :lipstick: 헤더 컴포넌트 완성

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg';
+
+export default function Header() {
+  return (
+    <header>
+      <div className={styles["header-wrap"]}>
+        <h1>
+          <Link to="./">
+            <img className={styles.logo} src={logo} alt="두근두근 비밀일기" />
+          </Link>
+        </h1>
+        <div>
+          {/* 공통 스타일 : 재사용되는 부분이라 로그인 버튼은 index.js에서 불러오고 있다 */}
+          <Link to="./signup" className="btn-join">회원가입</Link>
+        </div>
+      </div>
+    </header>
+  )
+}
