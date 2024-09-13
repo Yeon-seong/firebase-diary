45a484aa989f28022fa74cfa8a98fb5c42fb0377
yeon-seong <dd300@naver.com>
2024-09-13 12:57:09 +0900

    :lipstick: index.css, normalize를 통한 공통 css 설정 및 이미지 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           {/* root 경로 지정 : 현재 URL 창의 경로 */}
           <Route path='/' element={<Home />}></Route>
