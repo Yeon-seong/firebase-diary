d0a78bb119a5d849bfc25b1f753efcbcfa10e034
yeon-seong <dd300@naver.com>
2024-09-13 11:56:40 +0900

    :truck: 페이지 컴포넌트 생성 및 라우터 경로 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+
 function App() {
   return (
     <div>
-      hello world
+      <BrowserRouter>
+        <Routes>
+          {/* root 경로 지정 : 현재 URL 창의 경로 */}
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
