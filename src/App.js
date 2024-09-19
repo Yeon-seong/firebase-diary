091c3d81c82027db63f6913e52bd165436c2d5d0
yeon-seong <dd300@naver.com>
2024-09-20 05:47:22 +0900

    :zap: 파이어베이스의 사용자 정보를 불러오고 나서 화면이 렌더링 되도록 제작

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
-import Header from './components/Header';
+import Header from './components/header/Header';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+
+  const { isAuthReady } = useAuthContext();
+
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          {/* root 경로 지정 : 현재 URL 창의 경로 */}
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/signup' element={<Signup />}></Route>
-        </Routes>
-      </BrowserRouter>
+      {/* isAuthReady가 true라면 아래 내용을 반환 */}
+      {isAuthReady ?
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}>
+            </Route>
+            <Route
+              path="/login"
+              element={<Login />}>
+            </Route>
+            <Route
+              path="/signup"
+              element={<Signup />}>
+            </Route>
+          </Routes>
+        </BrowserRouter> : '로딩중입니다...'
+      }
     </div>
   );
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { appAuth } from '../firebase/config';
 
 
 // AuthContext 컨텍스트
@@ -12,8 +13,10 @@ const authReducer = (state, action) => {
       return { ...state, user: action.payload }
     case 'logout':
       return { ...state, user: null }
+    case 'authIsReady':
+      return { ...state, user: action.payload, isAuthReady: true }
     default:
-      return state;
+      return state
   }
 };
 
@@ -21,9 +24,33 @@ const authReducer = (state, action) => {
 // AuthContextProvider 컴포넌트
 const AuthContextProvider = ({ children }) => {
 
+  /*
+    사용자의 로그인 상태를 관찰하는 옵저버를 붙인다
+    파이어베이스의 사용자의 정보(로그인) 상태를 관찰하기 위해
+    onAuthStateChanged 함수를 사용한다
+
+    onAuthStateChanged : 사용자의 인증정보 변화를 관찰하는 함수다
+    onAuthStateChanged 함수는 Unsubscribe 함수를 반환하며,
+    더 이상 사용자의 변화를 관찰하지 않도록 하는 함수다
+    새로고침 후 초기에 딱 한번 실행하면 되기 때문에 이후에는 구독을 중지한다
+  */
+  useEffect(() => {
+    const unsubscribe = appAuth.onAuthStateChanged((user) => {
+      dispatch({ type: 'authIsReady', payload: user })
+    });
+    // 클린업 함수로 구독을 취소하도록 만든다.
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  // console.log('context: ', state);
+
+
   // useReducer는 상태와 디스패치 함수를 반환한다
-  const [state, dispatch] = useReducer(authReducer, {user: null});
-  console.log('context: ', state);
+  // isAuthReady : 사용자의 정보가 우리 애플리케이션으로 들어갔는지 유무 판단
+  const [state, dispatch] = useReducer(
+    authReducer, { user: null, isAuthReady: false }
+  );
   return (
     // 전역으로 관리될 value
     <AuthContext.Provider value={{ ...state, dispatch }}>
@@ -32,4 +59,4 @@ const AuthContextProvider = ({ children }) => {
   );
 };
 
-export {AuthContext, AuthContextProvider};
+export { AuthContext, AuthContextProvider };
