9a47283dcea2dbd6792fe4b9273e761228b4a4bf
yeon-seong <dd300@naver.com>
2024-09-20 05:22:03 +0900

    :pencil2: 헤더 메뉴바의 회원가입, 로그아웃 버튼이 여러 개로 잘못 표시되는 문제 수정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link, useLocation } from 'react-router-dom';
-import logo from '../assets/logo.svg';
+import logo from '../../assets/logo.svg';
 import { useLogout } from '../../hooks/useLogout';
 import { useAuthContext } from '../../hooks/useAuthContext';
 
@@ -24,34 +24,30 @@ export default function Header() {
           </Link>
         </h1>
         <div>
-          {/* ----- 만약 유저의 상태가 null, 즉 로그아웃 이라면 ----- */}
+          {/* ----- 만약 사용자의 상태가 null, 즉 로그아웃 이라면 ----- */}
           {!user && (
             <>
               {
                 /*
                   사용자 정보가 없고, 회원가입 화면이라면 로그인 버튼을 노출,
-                  로그인 화면이라면 회원가입 버튼을 노출한다.
+                  로그인 화면 이라면 회원가입 버튼을 노출한다
                 */
                 location.pathname === "/signup"
-                ? (<Link to="/login" className="btn-login">로그인</Link>)
-                : (<Link to="/signup" className="btn-join">회원가입</Link>)
+                ? <Link to="/login" className="btn-login">로그인</Link>
+                : <Link to="/signup" className="btn-join">회원가입</Link>
               }
             </>
           )}
-          {/* ----- 만약 유저의 상태가 null 이 아니라면 ----- */}
+          {/* ----- 만약 사용자의 상태가 null 이 아니라면 ----- */}
           {!user && (
             <>
-              {/*
-                사용자 정보가 없고, 회원가입 화면이라면 로그인 버튼을 노출,
-                로그인 화면이라면 회원가입 버튼을 노출한다.
-              */}
-              <p className="hello">환영합니다 <strong>{user.displayName}</strong>님!</p>
-              <Link to="/" className="btn-logout" onClick={logout}>로그아웃</Link>
+              {/* 공통 스타일 : 재사용되는 부분이라 로그인 버튼은 index.js에서 불러오고 있다 */}
+              <p className="hello">
+                환영합니다 <strong>{user}</strong>님!
+              </p>
+              <Link to="/" className="btn-join" onClick={logout}>로그아웃</Link>
             </>
           )}
-          {/* 공통 스타일 : 재사용되는 부분이라 로그인 버튼은 index.js에서 불러오고 있다 */}
-          <Link to="./signup" className="btn-join">회원가입</Link>
-          <Link to="/" className="btn-join" onClick={logout}>로그아웃</Link>
         </div>
       </div>
     </header>
