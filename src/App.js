9dcb21194efb584207808fec8077619ad32c929b
yeon-seong <dd300@naver.com>
2024-09-20 05:58:37 +0900

    :lipstick: 조건에 맞는 Route 화면 보여주기 사용자가 로그인이 되었을 경우, 안되어 있을 경우 보여주는 페이지 정하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
@@ -7,7 +7,8 @@ import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
 
-  const { isAuthReady } = useAuthContext();
+  // 통신이 완료되었다면 true를 반환
+  const { isAuthReady, user } = useAuthContext();
 
   return (
     <div>
@@ -16,17 +17,18 @@ function App() {
         <BrowserRouter>
           <Header />
           <Routes>
+            {/* root 경로 지정 : 현재 URL 창의 경로 */}
             <Route
-              path="/"
-              element={<Home />}>
+              path='/'
+              element={user ? <Home /> : <Navigate replace={true} to='/login' />}>
             </Route>
             <Route
-              path="/login"
-              element={<Login />}>
+              path='/login'
+              element={!user ? <Login /> : <Navigate replace={true} to='/' />}>
             </Route>
             <Route
-              path="/signup"
-              element={<Signup />}>
+              path='/signup'
+              element={!user ? <Signup /> : <Navigate replace={true} to='/' />}>
             </Route>
           </Routes>
         </BrowserRouter> : '로딩중입니다...'
