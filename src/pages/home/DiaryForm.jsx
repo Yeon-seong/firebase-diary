bf9552a2ecc3f875dfa6473548be9e38fda56710
yeon-seong <dd300@naver.com>
2024-09-20 09:41:00 +0900

    :white_check_mark: 일기 제목과 내용 작성 시 입력한 값이 잘 들어가는지 확인 성공

--- src/pages/home/DiaryForm.jsx
+++ src/pages/home/DiaryForm.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Home.module.css';
 
-
 export default function DiaryForm() {
-  return (
-    <form>
-      <label className="a11y-hidden" htmlFor="diary-title">
-        일기 제목
-      </label>
-      <input className="input-style" id="diary-title" type="text" placeholder="제목" required />
 
-      <label className="a11y-hidden" htmlFor="diary-content">
-        일기 내용
-      </label>
-      <textarea className="diary-textarea" id="diary-content" placeholder="오늘의 비밀은 무엇인가요?"></textarea>
-      
-      <button className="black-btn" type="submit">
-        작성하기
-      </button>
-    </form>
-  )
+const [title, setTitle] = useState('');
+const [secret, setSecret] = useState('');
+
+
+const handleData = (event) => {
+  if (event.target.type === 'text') {
+    setTitle(event.target.value);
+  } else {
+    setSecret(event.target.value);
+  }
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(title, secret);
+}
+
+return (
+  <form onSubmit={handleSubmit}>
+    <label className="a11y-hidden" for="diary-title">일기 제목</label>
+    <input className="input-style" id="diary-title" type="text" placeholder="제목" required onChange={handleData} value={title} />
+
+    <label className="a11y-hidden" for="diary-content">일기 내용</label>
+    <textarea className={styles["diary-textarea"]} id="diary-content" placeholder="오늘의 비밀은 무엇인가요?" onChange={handleData} value={secret}></textarea>
+    <button className="black-btn" type="submit">작성하기</button>
+  </form>
+)
 }
